7ba84228e342e7c1e68aad219fd99c27c271c08c
hfawazbc <hfawaz2520@gmail.com>
2020-10-12 18:47:32 -0700

    Hooked backend with frontend using express

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./frontend/build'));
+}
+
 app.use(session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
